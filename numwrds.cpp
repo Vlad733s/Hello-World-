813a69d04ab8d3d1554a9cd8db7067f11460b6fc
Vlad733s <stepaniuk_ba18@nuwm.edu.ua>
2021-03-03 17:02:33 +0200

    numwrds.cpp

---
+++ numwrds.cpp
@@ -0,0 +1,12 @@
+-h(numwrds.exe -h)
+bash: wq: command not found
+bash: wq!: command not found
+bash: qa: command not found
+bash: wq: command not found
+bash: x: command not found
+bash: :x: command not found
+
+bash: EDF: command not found
+S
+
+
